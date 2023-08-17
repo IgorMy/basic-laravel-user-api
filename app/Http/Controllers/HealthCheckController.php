c548cfc7072f6682016f6730f92a33e9a564a119
Ihar Myshkevich <33940074+IgorMy@users.noreply.github.com>
2023-08-17 18:57:45 +0200

    little forgotten things to add to HealthCheck

--- app/Http/Controllers/HealthCheckController.php
+++ app/Http/Controllers/HealthCheckController.php
@@ -1,11 +1,13 @@
 <?php
 
+declare (strict_types=1);
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class HealthCheckController extends Controller
+final class HealthCheckController extends Controller
 {
     public function __invoke(Request $request): Response
     {
