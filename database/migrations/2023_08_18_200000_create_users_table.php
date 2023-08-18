049e0b52070746dd739c5542df18fc501a7b9c5e
Ihar Myshkevich <33940074+IgorMy@users.noreply.github.com>
2023-08-18 21:30:00 +0200

    migrations created

--- database/migrations/2023_08_18_200000_create_users_table.php
+++ database/migrations/2023_08_18_200000_create_users_table.php
@@ -6,20 +6,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->ulid('UsersUlid')->primary();
+            $table->string('username',255);
+            $table->string('email')->unique()->index();
+            $table->string('password',1000);
+            $table->ulid('RoleUlid')->index();
+            $table->foreign('RoleUlid')->references('RoleUlid')->on('role');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
