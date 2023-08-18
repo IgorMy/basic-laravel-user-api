bf3117fa8f454f3361ea07d9ae7f6837e152d051
Ihar Myshkevich <33940074+IgorMy@users.noreply.github.com>
2023-08-18 21:45:50 +0200

    basic model configureation

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
-class User extends Model
+class Role extends Model
 {
-    use HasFactory;
     use HasUlids;
 
     /**
@@ -19,14 +16,14 @@ class User extends Model
      *
      * @var string
      */
-    protected $table = 'users';
+    protected $table = 'role';
 
     /**
      * The primary key associated with the table.
      *
      * @var string
      */
-    protected $primaryKey = 'UsersUlid';
+    protected $primaryKey = 'RoleUlid';
 
     /**
      * Indicates if the model's ID is auto-incrementing.
@@ -43,39 +40,20 @@ class User extends Model
     protected $keyType = 'string';
 
     /**
+     * The attributes that should be visible in arrays.
+     *
+     * @var array
+     */
+    protected $visible = ['title','created_at'];
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'title',
     ];
+    
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-    ];
-
-    /**
-     * The attributes that should be visible in arrays.
-     *
-     * @var array
-     */
-    protected $visible = ['username','email','created_at'];
-
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'created_at' => 'datetime',
-    ];
 }
